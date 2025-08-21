a80a4b3f265f7ebf0908d5cc778ff36d7d322a42
Kartik Walia <kartikkwalia2006@gmail.com>
2025-08-21 09:46:35 +0530

    Update user.route.js

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -4,7 +4,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", protectRoute, getAllUsers);
+router.get("/", getAllUsers); //, protectRoute
 // router.get("/messages/:userId", protectRoute, getMessages);
 
 export default router;
